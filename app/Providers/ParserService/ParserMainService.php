7e3b68e7b3997a05650dc22e76377b715b122d7b
Bodo8 <b.trojanski@gmail.com>
2021-05-24 17:27:56 +0200

    refracted method in ParserMainService.php

--- app/Providers/ParserService/ParserMainService.php
+++ app/Providers/ParserService/ParserMainService.php
@@ -25,11 +25,18 @@ class ParserMainService
 
     private ParserStringService $parserStringService;
 
+    /**
+     * ParserMainService constructor.
+     */
     public function __construct()
     {
         $this->parserStringService = new ParserStringService();
     }
 
+    /**
+     * @param string $innerBody
+     * @return array
+     */
     public function cleanHtmlStringsFromBadTags(string $innerBody): array
     {
         $bodiesFromBody = $this->parserStringService->changeStringHtmlToArray($innerBody);
@@ -38,38 +45,21 @@ class ParserMainService
             $badTags = true;
 
             while ($badTags) {
-                $startNumber = $this->parserStringService->searchBadTag(
-                    $bodiesFromBody, $TAG['BEGINNING']
-                );
-
-                $endNumber = $this->parserStringService->searchBadTag(
-                    $bodiesFromBody, $TAG['END']
-                );
+                $startNumber = $this->getStartNumber($bodiesFromBody, $TAG);
+                $endNumber = $this->getEndNumber($bodiesFromBody, $TAG);
 
                 if ($endNumber < $startNumber) {
-                    $endNumber = $this->parserStringService->searchEndSpecialTag(
-                        $bodiesFromBody, $TAG['END'], $startNumber
-                    );
+                    $endNumber = $this->getSpecialEndNumber($bodiesFromBody, $TAG, $startNumber);
                 }
 
                 if ($startNumber < count($bodiesFromBody)
                     && $endNumber < count($bodiesFromBody)) {
 
-                    $startTagToRemove = $bodiesFromBody[$startNumber];
-                    $cleanWord = $this->parserStringService->RemoveTagFromOneString(
-                        $startTagToRemove, $TAG['BEGINNING']
-                    );
-                    $bodiesFromBody[$startNumber] = $cleanWord;
-
-                    $endTagToRemove = $bodiesFromBody[$endNumber];
-                    $cleanWord = $this->parserStringService->RemoveTagFromOneString(
-                        $endTagToRemove, $TAG['END']
-                    );
-                    $bodiesFromBody[$endNumber] = $cleanWord;
+                    $bodiesFromBody = $this->removeTagsFromOneWord(
+                        $bodiesFromBody, $startNumber, $TAG, $endNumber);
 
                     $bodiesFromBody = $this->parserStringService->removePartArray(
-                        $bodiesFromBody, $startNumber, $endNumber
-                    );
+                        $bodiesFromBody, $startNumber, $endNumber);
 
                 } else {
                     $badTags = false;
@@ -77,7 +67,70 @@ class ParserMainService
             }
         }
 
+        return $bodiesFromBody;
+    }
+
+    /**
+     * @param array $bodiesFromBody
+     * @param int $startNumber
+     * @param $TAG
+     * @param int $endNumber
+     * @return array
+     */
+    private function removeTagsFromOneWord(
+        array $bodiesFromBody, int $startNumber, array $TAG, int $endNumber
+    ): array
+    {
+        $startTagToRemove = $bodiesFromBody[$startNumber];
+        $cleanWord = $this->parserStringService->RemoveTagFromOneString(
+            $startTagToRemove, $TAG['BEGINNING']
+        );
+        $bodiesFromBody[$startNumber] = $cleanWord;
+
+        $endTagToRemove = $bodiesFromBody[$endNumber];
+        $cleanWord = $this->parserStringService->RemoveTagFromOneString(
+            $endTagToRemove, $TAG['END']
+        );
+        $bodiesFromBody[$endNumber] = $cleanWord;
 
         return $bodiesFromBody;
+    }
+
+    /**
+     * @param array $bodiesFromBody
+     * @param $TAG
+     * @return int
+     */
+    private function getStartNumber(array $bodiesFromBody, $TAG): int
+    {
+        $startNumber = $this->parserStringService->searchBadTag(
+            $bodiesFromBody, $TAG['BEGINNING']);
+
+        return $startNumber;
+    }
+
+    /**
+     * @param array $bodiesFromBody
+     * @param $TAG
+     * @return int
+     */
+    private function getEndNumber(array $bodiesFromBody, $TAG): int
+    {
+        $endNumber = $this->parserStringService->searchBadTag(
+            $bodiesFromBody, $TAG['END']);
+        return $endNumber;
+    }
+
+    /**
+     * @param array $bodiesFromBody
+     * @param $TAG
+     * @param int $startNumber
+     * @return int
+     */
+    private function getSpecialEndNumber(array $bodiesFromBody, $TAG, int $startNumber): int
+    {
+        $endNumber = $this->parserStringService->searchEndSpecialTag(
+            $bodiesFromBody, $TAG['END'], $startNumber);
+        return $endNumber;
     }
 }
